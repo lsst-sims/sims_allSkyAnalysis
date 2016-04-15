faad002d3860c0828a9a6a6f37ee4f67fcb8a344
Peter Yoachim <yoachim@uw.edu>
2016-04-15 12:26:46 -0700

    switch to flux differencing

--- python/makemovie.py
+++ python/makemovie.py
@@ -12,7 +12,7 @@ from utils import robustRMS
 
 if __name__ == '__main__':
 
-	nframes = 400
+	nframes = 5000
 	nstart = 500
 
 	outdir = 'MoviePlots'
@@ -44,6 +44,9 @@ if __name__ == '__main__':
 
 	outlier_mag = 0.1
 	alt_limit = np.radians(23.) 
+	fracs_out = []
+	med_diff1 = []
+	med_diff2 = []
 
 	for i,(mjd, stats) in enumerate(zip(umjd[nstart:nstart+nframes], 
 	                                    frame_stats[nstart:nstart+nframes])):
@@ -54,7 +57,7 @@ if __name__ == '__main__':
 		fig = plt.figure()
 		frame = single_frame(mjd)
 		# Need to crop off high airmass pixels. use stupid_fast
-		diff = frame - previous
+		diff = (10.**(-0.4*frame) / 10.**(-.4*previous)) - 1.
 		previous = frame.copy()
 		out = np.where((np.isnan(frame)) | (np.isnan(previous)) | (frame == hp.UNSEEN) | 
 		               (previous == hp.UNSEEN) | (alt < np.radians(10.)))
@@ -63,19 +66,28 @@ if __name__ == '__main__':
 		# maybe rotate based on LMST and latitude?
 		gdiff = np.where( (diff != hp.UNSEEN) & (np.isnan(diff) == False))[0]
 		rms = robustRMS(diff[gdiff])
-		nout = np.size(np.where( (np.abs(diff[gdiff]) > outlier_mag) & (alt[gdiff] > alt_limit))[0])
-		nout = nout/float(np.size(gdiff))*100
+		nout = np.size(np.where( (np.abs(diff[gdiff] - np.median(diff[gdiff]) ) > outlier_mag) & (alt[gdiff] > alt_limit))[0])
+		nout = nout/float(np.size(np.where(alt[gdiff] > alt_limit)[0]))*100
+		fracs_out.append(nout)
 		hp.mollview(frame, sub=(2,2,1), rot=(lmst, site.latitude,0), min=-7, max=-2.5, unit='mag', 
 		            title='%.2f' % mjd)
-		hp.mollview(diff, sub=(2,2,2), min=-0.25, max=0.25,  rot=(lmst, site.latitude,0), 
-		            cmap=RdBu, unit='(mag)', 
-		            title=r'$\sigma$=%.2f, Nout=%i' % (rms, nout))
-		diff = frame - median_r
+		hp.mollview(diff, sub=(2,2,2), min=-.3, max=.3,  rot=(lmst, site.latitude,0), 
+		            cmap=RdBu, unit='(flux)', 
+		            title=r'$\sigma$=%.2f, percent out=%i' % (rms, nout))
+		diff2 = (10.**(-.4*frame)/10.**(-.4*median_r)) -1.
 		out = np.where((frame == hp.UNSEEN) | (median_r == hp.UNSEEN) | (alt < np.radians(10.)))
+		diff2[out] = hp.UNSEEN
+		hp.mollview(diff2, sub=(2,2,3), min=-2, max=2, rot=(lmst, site.latitude,0), 
+		            cmap=RdBu, unit='(flux)', title='')
+		diff_correlation = (diff*diff2)**2
+		out = np.where((diff == hp.UNSEEN) | (diff2 == hp.UNSEEN))
 		diff[out] = hp.UNSEEN
-		hp.mollview(diff, sub=(2,2,3), min=-2, max=2, rot=(lmst, site.latitude,0), 
-		            cmap=RdBu, unit='(mag)', title='')
-
+		diff2[out] = hp.UNSEEN
+		good = np.where(diff != hp.UNSEEN)
+		diff_correlation = ((diff-np.median(diff[good]))*(diff2-np.median(diff2[good])))**2
+		diff_correlation[out] = hp.UNSEEN
+		hp.mollview(diff_correlation, sub=(2,2,4), min=0, max=1, rot=(lmst, site.latitude,0), 
+		            unit='(power)', title='correlation')
 		#sm, sm_diff = starmap(mjd)
 		#hp.mollview(sm, sub=(2,2,3), rot=(lmst, site.latitude,0), unit='N stars', title='')
 		#hp.mollview(sm_diff, sub=(2,2,4), rot=(lmst, site.latitude,0), unit='N stars', title='')
@@ -83,7 +95,6 @@ if __name__ == '__main__':
 		fig.savefig('%s/%04i_.png' % (outdir, i))
 		plt.close(fig)
 		
-
 	# then just call ffmpeg like here: https://trac.ffmpeg.org/wiki/Create%20a%20video%20slideshow%20from%20images
 	# open with vLC
 	#  ffmpeg -framerate 10 -pattern_type glob -i '*.png'  out.mp4

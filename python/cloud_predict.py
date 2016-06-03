7762303db46b50c69260ba975ba739eece48aff6
Peter Yoachim <yoachim@uw.edu>
2016-06-03 16:05:18 -0700

    experimenting

--- python/cloud_predict.py
+++ python/cloud_predict.py
@@ -19,10 +19,25 @@ def diff_hp(frame1,frame2, norm=True):
     return result
 
 
+def screen2hp(nside, npix=1000, height=500.):
 
-def hp2xyz(nside, hpInd, height):
+    # generate a screen
+    xx, yy = np.meshgrid(np.arange(-npix, npix, 1), np.arange(-npix, npix, 1), indexing='ij')
+    r = (xx**2+yy**2)**0.5
+    az = np.arctan2(yy,xx)
+    alt = np.arctan(height/r)
+
+    # Ah, I can convert these alt,az coords to ra,dec, then there's no problem using an ra,dec cloud map.
+
+    hpids = hp.ang2pix(nside, alt, az)
+    return hpids
+
+
+
+
+def hp2screen(nside, hpInd, height):
     """
-    Convert a healpy map to xyz coordinates
+    Convert a healpy map to a flat screen
     """
     
     # XXX--check that this is right!!
@@ -47,12 +62,7 @@ def propigateClouds(cloudmask, height, velocity, dt):
     xnew = x*velocity[0]*dt
     ynew = y*velocity[1]*dt
 
-    # convert x,y,z to alt, az
-
-    # convert alt az to hpmap
-
-    # 
-
+    
 
 def predictCloudMotion(cloudMask, diffTime):
     """
@@ -83,10 +93,18 @@ if __name__ == '__main__':
     sun_alts = skyMaps['sunAlts'][good].copy()
     moon_alts = skyMaps['moonAlts'][good].copy()
 
+    startnum = 300
 
-    diff = diff_hp(single_frame(umjd[251]), single_frame(umjd[250]))
+    diff = diff_hp(single_frame(umjd[startnum+1]), single_frame(umjd[startnum]))
     cloudyArea, cloudmask = cloudyness(diff)
+    # ah, cloudmask is in ra,dec.  Need to rotate os that we're in alt-az!
+
     nside = hp.npix2nside(cloudmask.size)
+    hpids = screen2hp(nside)
+
+    screen = cloudmask[hpids]
+
+
     high = hp.pixelfunc.ud_grade(cloudmask, nside_out=nside*2)
     lower = hp.pixelfunc.ud_grade(high, nside_out=nside)
 

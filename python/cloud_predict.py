7ef08c2601928f8cce75e5a21a924576ac5f8504
Peter Yoachim <yoachim@uw.edu>
2016-06-10 16:24:22 -0700

    sketching out class

--- python/cloud_predict.py
+++ python/cloud_predict.py
@@ -5,6 +5,7 @@ from medDB import single_frame
 from lsst.sims.skybrightness import stupidFast_altAz2RaDec, stupidFast_RaDec2AltAz
 from lsst.sims.utils import _raDec2Hpid, _hpid2RaDec, Site
 from scipy import interpolate
+import warnings
 
 site = Site('LSST')
 lat = site.latitude_rad
@@ -41,7 +42,7 @@ def screen2hp(nside, mjd, npix=1000, height=500.):
 
 
 
-def hp2screen(inmap, mjd, height=500, alt_limit=10.):
+def hp2screen(inmap, mjd, height=500, alt_limit=10., npts=600, grid_x=None, grid_y=None):
     """
     Convert a healpy map to a flat screen at height h
     """
@@ -54,7 +55,15 @@ def hp2screen(inmap, mjd, height=500, alt_limit=10.):
     x = r*np.cos(az[good])
     y = r*np.sin(az[good])
     z = inmap[unmasked][good]
-    return x,y,z
+
+    if grid_x is None:
+        grid_x, grid_y = np.mgrid[x.min():x.max():600j, y.min():y.max():600j]
+    screen_grid = interpolate.griddata(np.vstack((x, y)).T, z, (grid_x, grid_y), method='nearest')
+
+    # Maybe compute the alt-az of the grid_x, grid_y so that it'll be fast to convert back to ra, dec? 
+    # Pretty clear this should be a class rather than a bunch of functions.
+
+    return grid_x, grid_y, screen_grid
 
 
 
@@ -89,9 +98,69 @@ def predictCloudMotion(cloudMask, diffTime):
         The time gap between exposures.  Seconds.
     """
 
-    
 
 
+class CloudMotion(object):
+    """
+    Take two maps and predict where the clouds will be in the future
+    """
+
+    def __init__(self, nside=64):
+        """
+        Set up the resolution of various things?
+        """
+
+        self.predictLimit = 20.  # minutes
+
+    def set_maps(self, map1,map2, mjd1,mjd2):
+        """
+        set the two cloud maps to use to generate cloud motion predictions
+        """
+
+        # Put things in correct order
+        if mjd2 < mjd1:
+            self.mjd1 = mjd2
+            self.mjd2 = mjd1
+            self.map2 = map1
+            self.map1 = map2
+        else:
+            self.mjd1 = mjd1
+            self.mjd2 = mjd2
+            self.map2 = map2
+            self.map1 = map1
+
+        self.dt = self.mjd2-self.mjd1
+
+        # Diff the maps, project to a screen.
+        # hmm, should probably change this to be a diff on screen rather than on sphere. 
+        diff = diff_hp(single_frame(umjd[startnum+1]), single_frame(umjd[startnum]))
+        cloudyArea, cloudmask = cloudyness(diff)
+
+        # Find the best shift, dx, dy
+
+
+    def forecast(self, mjd, usemap='both'):
+        """
+        Return a healpix map that is an ra,dec cloud mask prediction at input mjd
+
+        mjd : float
+            The Modified Julian Date to predict the cloud cover too
+        usemap : str (map1, map2, both)
+            Which input map should be propigated forward in time.  Default is to be conservative 
+            and propigate all pixels flagged as cloudy in both maps.
+        """
+
+        dt2 = (mjd-self.mjd2)*24*60
+        if  dt2 > self.predictLimit:
+            warnings.warn('Time gap of %f minutes is greater than limit %f minutes' % (dt2, self.predictLimit))
+
+
+
+
+
+        # What is the best way to test the forecast? Maybe say what fraction of sky is cloudy vs what fraction of 
+        # predicted clear space is cloudy?  Above some airmass limit.  Also a stat on how much clear sky was lost
+        # to falsly predicted cloudy area.
 
 
 if __name__ == '__main__':
@@ -115,9 +184,11 @@ if __name__ == '__main__':
     #hpids = screen2hp(nside)
     #screen = cloudmask[hpids]
 
-    x, y, z = hp2screen(cloudmask, umjd[startnum])
+    grid_x, grid_y, screen_grid = hp2screen(cloudmask, umjd[startnum])
 
-    H, xe, ye = np.histogram2d(x,y, bins=1000, weights=z)
+
+
+    #H, xe, ye = np.histogram2d(x,y, bins=1000, weights=z)
 
     # OK, I can interpolate this to a meshgrid, then cross-correlate the plus and minus.  
     #screenInterp = interpolate.interp2d(x, y, z, kind='linear')
